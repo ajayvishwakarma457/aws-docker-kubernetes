0a0987f59a5f1bcd79d5884ee3a0581a9c26a0d5
Ajay <ajayvishwakarma457@gmail.com>
2025-09-07 16:21:19 +0530

    changed in / route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('App Runner');
+  res.send('App Runner 1');
 });
  
  app.get('/home', (req, res) => {
